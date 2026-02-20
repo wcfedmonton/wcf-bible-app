50066e12c52c16892f4b4e2b538c9fa0a995a11f
Jospin <jospin.muhanuzi@gmail.com>
2026-02-20 15:07:18 -0700

    Implement server-side loading for initial page rendering and update fetchChapter to support custom fetch function

--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	import Search from './components/home/Search.svelte';
 	import Verse from './components/home/VerseNavigator.svelte';
 
@@ -8,21 +6,17 @@
 	import { fetchChapter } from '$lib/bible/chapterServices';
 	import { type BibleTranslation, type Verse as VerseType } from '$lib/server/bible';
 
+	const { data: initialData } = $props(); // initial data is loaded on the server
+	
 	let state: {
 		verseLimit: number;
-		verseReference: string;
 		osis?: OSISReference;
+		verseReference: string;
 		verseData: VerseType[];
 		selectedVerseIndex: number;
 		translation: BibleTranslation;
-	} = $state({
-		verseLimit: 1,
-		verseReference: '',
-		osis: undefined,
-		verseData: [],
-		selectedVerseIndex: 0,
-		translation: 'NIV'
-	});
+	// svelte-ignore state_referenced_locally
+		} = $state(initialData);
 
 	async function fetchChapterData(query: string) {
 		const updatedData = await fetchChapter({
@@ -38,8 +32,6 @@
 			state.selectedVerseIndex = updatedData.selectedVerseIndex;
 		}
 	}
-
-	onMount(async () => fetchChapterData('John 1:1')); // this will be the default verse
 </script>
 
 <h1>Welcome to SvelteKit</h1>
