bf99cd6f6605a4375c5d7f233744943d045a13eb
Jospin <jospin.muhanuzi@gmail.com>
2026-03-01 18:57:28 -0700

    added loading and error handling states to registration process

--- code/src/routes/components/authentication/Input.svelte
+++ code/src/routes/components/authentication/Input.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Title from './Title.svelte';
 
-	let { title, value = $bindable() } = $props();
+	let { title, value = $bindable(), error = $bindable() } = $props();
 </script>
 
 <div class="mb-5">
@@ -12,4 +12,10 @@
 		name={title}
 		class="w-full bg-form_input border border-input_border border-solid rounded py-2 px-4 hover:border-input_focus focus:outline-none focus:ring-0 focus:input-focus"
 	/>
+
+	{#if error}
+		<div class="py-2">
+			<p class="absolute text-[0.8rem] text-error_red">{error}</p>
+		</div>
+	{/if}
 </div>
--- code/src/routes/register/+page.svelte
+++ code/src/routes/register/+page.svelte
@@ -21,23 +21,36 @@
 		password: ''
 	});
 
+	let errorMessage = $state('');
+	let loading = $state(false);
+
 	const { form } = createForm({
 		onSubmit: async (values) => {
-			const params = new URLSearchParams({ 
-				name: values['Full Name'], 
+			loading = true;
+			errorMessage = '';
+
+			const params = new URLSearchParams({
+				name: values['Full Name'],
 				email: values['Email Address'],
 				password: values.password
 			});
 
-			await fetch(`api/register?${params.toString()}`, { method: "POST" });
+			const res = await fetch(`api/register?${params.toString()}`, { method: 'POST' });
+			const data = await res.json();
+
+			if (!res.ok) {
+				errorMessage = data.error;
+			}
+
+			loading = false;
 		}
 	});
 
 	function disabledCondition(state: FormState) {
-		const emptyField = Object.values(state).some((field) => field === '');
 		const invalidEmail = !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(state.email);
+		const emptyField = Object.values(state).some((field) => field === '');
 
-		return emptyField || invalidEmail || state.password.length < 8;
+		return invalidEmail || emptyField || state.password.length < 8 || loading;
 	}
 </script>
 
@@ -46,12 +59,11 @@
 		<div class="flex items-center">
 			<AuthCard cardTitle={'Create Account'}>
 				<Input title={'Full Name'} bind:value={formState.full_name} />
-				<Input title={'Email Address'} bind:value={formState.email} />
+				<Input title={'Email Address'} bind:value={formState.email} bind:error={errorMessage} />
 				<Password bind:value={formState.password} />
-				<Button title={'Register'} bind:state={formState} {disabledCondition} />
+				<Button title={'Register'} bind:state={formState} bind:loading {disabledCondition} />
 				<Divider />
 				<GoogleButton action={'Sign up'} />
-
 				<div class="text-center mt-6">
 					Already have an account?
 					<a href="/login" class="text-input_focus">Sign In</a>
