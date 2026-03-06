43d89128a61dd060a3965a7b1f508427a438ef73
Jospin <jospin.muhanuzi@gmail.com>
2026-03-05 17:18:26 -0700

    Empty state page is finished

--- code/src/routes/components/sets/Button.svelte
+++ code/src/routes/components/sets/Button.svelte
@@ -2,7 +2,7 @@
     const { prompt, icon } = $props();
 </script>
 
-<button class="bg-[#d3413f2e] cursor-pointer flex flex-row justify-center p-[0.3rem] w-22 mt-2 border-solid border-1 rounded-[0.2rem] border-[#d3413f59] gap-[0.3rem]">
+<button class="bg-[#d3413f2e] cursor-pointer flex flex-row justify-center p-[0.3rem] w-22 mt-2 border-solid border-1 rounded-[0.2rem] border-action_btn_border gap-[0.3rem]">
     {@html icon}
 
     <p class="text-[0.7rem] text-[#e0e0e0]">{prompt}</p>
---
+++ code/src/routes/components/sets/Empty.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Clipboard from "$lib/assets/clipboard.svg"
+</script>
+
+<div class="flex flex-col justify-center items-center w-[72rem] h-dvh">
+    <div class="w-16 h-16 flex justify-center items-center rounded-[50%] bg-[#d3413f2e] border-solid border-1 border-action_btn_border">
+        <img src={Clipboard} alt="clipboard" class="w-7 h-7"/>
+    </div>
+
+    <p class="pt-4 font-serif text-[1.3rem] font-semibold">No verse sets yet</p>
+    <p class="w-100 flex text-center text-[#b0b5b1] pt-2">
+        Verse sets allow you to organize scripture for services, studies, or events. Create your first set to get started.
+    </p>
+</div>
