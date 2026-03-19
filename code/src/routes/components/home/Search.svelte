72b1a6bc6735e112cd52ad2a85cf19ba77183f77
Oghenerukevwe Ishomo <rukevweishomo@gmail.com>
2026-03-19 15:04:59 -0600

    Refactor Search.svelte with Tailwind CSS styles

--- code/src/routes/components/home/Search.svelte
+++ code/src/routes/components/home/Search.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import tailwindcss from '@tailwindcss/vite';
 	import { generateAutoSuggestions } from '$lib/bible/suggestionUtils';
 	import search_icon from '$lib/assets/OOjs_UI_icon_keyboard.svg';
 
@@ -49,12 +50,12 @@
 	}
 </script>
 
-<div class="topnav">
-	<div class="search-bar-container">
-		<div class="search-input-container">
+<div class="flex">
+	<div class="flex-col gap-(0.5rem) w-fit ml-(83%)">
+		<div class="relative">
 			<input
 				type="text"
-				class="search-bar"
+				class="z-0 text-auto fixed pt-(0.4rem) pb-(0.4rem) pl-(1.9rem) pr-(0.3rem) w-(13%) rounded-(1rem)"
 				bind:value={userInput}
 				onkeydown={handleKeyDown}
 				placeholder="start typing to find verse"
@@ -65,13 +66,13 @@
 					}
 				}}
 			>
-			<img class="search-icon" src={search_icon} alt="search icon"/> 
+			<img class="fixed z-1 py-(0.4rem) px-(0.5rem) scale-75" src={search_icon} alt="search icon"/> 
 			{#if showSuggestions}
-			<div class="suggestions-list" role="listbox">
+			<div class="fixed mt-(2.2rem) bg-gray w-(14.75%)" role="listbox">
 				{#each autoSuggestions as suggestion, index (suggestion)}
 					<div
 						role="option"
-						class="option"
+						class="cursor-pointer hover:selected bg-gray-200"
 						class:selected={index === highlightedIndex}
 						tabindex="-1"
 						aria-selected={index === highlightedIndex}
@@ -87,47 +88,3 @@
 		</div>
 		
 </div>
-
-<style>
-	.option {
-		cursor: pointer;
-	}
-	.suggestions-list {
-		position: fixed;
-		margin-top: 2.2rem;
-		background: grey;
-		width: 14.75%;
-	}
-	.option:hover,
-	.selected {
-		background: #dbeafe;
-	}
-	.search-bar-container {
-		display: flex;
-		flex-direction: column;
-		gap: 0.5rem;
-		width: fit-content;
-		margin-left: 83%;
-	}
-	.search-bar {
-		z-index: 0;
-		font-size: auto;
-		position: fixed;
-		padding-top: 0.4rem; 
-		padding-bottom: 0.4rem;
-		padding-left: 1.9rem;
-		padding-right: 0.3rem;
-		width: 13%;
-		border-radius: 1rem;
-	}
-	.search-icon {
-		z-index: 1;
-		position: fixed;
-		padding: 0.4rem 0.5rem;
-		transform: scale(75%);
-	}
-
-	.search-input-container {
-		position: relative;
-	}
-</style>
